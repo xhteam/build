5e8661f6fe164699e177492b8801fd0976d7ed03
raymond.wang <raymond1860@gmail.com>
2013-09-14 20:22:10 +0800

    Add TARGET_FORCE_ADB_NONSECURE config to enable adb non secure for user building

--- core/main.mk
+++ core/main.mk
@@ -287,7 +287,11 @@ enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
+  ifeq ($(TARGET_FORCE_ADB_NONSECURE),1)
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+  else
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  endif
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
